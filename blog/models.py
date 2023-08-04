7eab9422f6ad67047fdbe3122c42b86bedc3f0bd
Nobody12here <uraja01313@gmail.com>
2023-08-04 10:04:08 +0500

    Added pagination and dynamic urls

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 class PublishedManager(models.Manager):
@@ -13,7 +14,7 @@ class Post(models.Model):
 	published = PublishedManager()
 	#Basic Fields that every post should have
 	title = models.CharField(max_length=250)
-	slug = models.SlugField(max_length=250)
+	slug = models.SlugField(max_length=250,unique_for_date='publish')
 	body = models.TextField()
 	#Date and time the post was created,updated,published
 	created = models.DateTimeField(auto_now_add=True)
@@ -35,5 +36,11 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+	def get_absolute_url(self):
+		return reverse('blog:post_detail',args=
+			[self.publish.year,
+			self.publish.month,
+			self.publish.day,
+			self.slug])
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.http import Http404
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 def post_list(request):
 	 #Fetches the post irrespective of the status
-	posts = Post.published.all()
+	posts_lists = Post.published.all()
+	# 3 Posts per page
+	paginator = Paginator(posts_lists,3)
+	page_number = request.GET.get('page',1)
+	try:
+		posts = paginator.page(page_number)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+
+
+	
+
 	return render(request,'blog/post/list.html',{'posts':posts})
 
-def post_details(request,id):
+def post_details(request,year,month,day,post):
 	try:
-		post = get_object_or_404(Post,id=id) #Fetches the post with id irrespective of the status
+		post = get_object_or_404(Post,status=Post.Status.PUBLISHED,slug=post,
+			publish__year=year,publish__month=month,publish__day=day
+		) #Fetches the post with the given slug and publish date
 	except Post.DoesNotExist:
 		raise Http404('Post does not exists')
 	
